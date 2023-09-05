39079f5ee80b90526d353d7832756ea99c92ffea
mattfeng <a05460E974@1991>
2023-09-05 13:56:00 +0800

    ddd

---
+++ run.py
@@ -0,0 +1,11 @@
+from pywebcopy import save_website
+save_website(
+url="https://maize-bison-flvr81.mystrikingly.com/contact",
+project_folder="/Users/mattfeng/projects/bizhome",
+project_name="my_site",
+bypass_robots=True,
+debug=True,
+open_in_browser=True,
+delay=None,
+threaded=False,
+)
